e58ad237e56ec252b1fb084f1122ca47b6e6ff13
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2026-05-10 22:00:28 +0530

    Add The 67th Integer Problem [A] from Codeforces

---
+++ 2218/A - The 67th Integer Problem/solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            System.out.println(67);
+        }
+    }
+}
