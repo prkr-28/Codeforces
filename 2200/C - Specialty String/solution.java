2de70b3a500b46f10691c8f4c3333b310ea1cdc6
Priyanshu Kumar <148961152+prkr-28@users.noreply.github.com>
2026-05-16 19:24:58 +0530

    Add Specialty String [C] from Codeforces

---
+++ 2200/C - Specialty String/solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            Stack<Character> stack = new Stack<>();
+            for (char c : s.toCharArray()) {
+                if (!stack.isEmpty() && stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            if (stack.isEmpty()) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
